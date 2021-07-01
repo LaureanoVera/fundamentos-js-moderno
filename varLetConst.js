fb9d6d289c0030f182d77b54613e48c632368922
LaureanoVera <laureanoigvera@gmail.com>
2021-07-01 17:07:20 -0300

    var let const ej: 1

---
+++ varLetConst.js
@@ -0,0 +1,12 @@
+// var vs let vs const
+var edad = 17;
+var edad = 18;
+console.log(edad);
+
+let nombre = 'Lau';
+// let nombre = 'Laureano'; // <- error
+nombre = 'Laureano';
+console.log(nombre);
+
+const apellido = 'Vera';
+// apellido = 'Kaldi'; // <- error
